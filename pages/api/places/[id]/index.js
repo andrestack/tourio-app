6330e05ad6ef92046d84ab09ecade62b8dfddace
André Silva <andre.silva@posteo.de>
2023-04-13 17:23:11 +0200

    Add update function

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,14 +1,20 @@
-import dbConnect from "../../../db/connect";
+import dbConnect from "../../../../db/connect";
 import Location from "../../../../db/models/Location";
 
 export default async function handler(request, response) {
   await dbConnect();
-  // const { id } = request.query;
+  const { id } = request.query;
 
   switch (request.method) {
     case "GET":
       const location = await Location.findById(id);
       response.status(200).json(location);
+      break;
+    case "PUT":
+      const locationToUpdate = await Location.findByIdAndUpdate(id, {
+        $set: request.body,
+      });
+      response.status(200).json(locationToUpdate);
       break;
     // case "POST":
     //   try {
