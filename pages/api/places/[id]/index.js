7ea0b08aedbf0bbefb48ccf2e00253046835de63
christoph.raddatz <hello@chrstphrddtz.com>
2023-04-13 16:21:41 +0200

    Add Form functionality

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -3,16 +3,26 @@ import Location from "../../../../db/models/Location";
 
 export default async function handler(request, response) {
   await dbConnect();
-  const { id } = request.query;
+  // const { id } = request.query;
 
   switch (request.method) {
     case "GET":
       const location = await Location.findById(id);
       response.status(200).json(location);
       break;
+    // case "POST":
+    //   try {
+    //     const locationData = request.body;
+    //     const location = new Location(locationData);
+    //     await location.save();
+    //     response.status(201).json({ status: "product created" });
+    //   } catch (e) {
+    //     console.error(e);
+    //     response.status(400).json({ error: e.message });
+    //   }
+    //   break;
 
     default:
       return response.status(404).json({ status: "Not found" });
   }
-
 }
--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,17 +1,27 @@
-import dbConnect from "../../../db/connect"
+import dbConnect from "../../../db/connect";
 import Location from "../../../db/models/Location";
 
 export default async function handler(request, response) {
-  await dbConnect()
+  await dbConnect();
 
   switch (request.method) {
     case "GET":
-      const location = await Location.find()
+      const location = await Location.find();
       response.status(200).json(location);
       break;
-  
+    case "POST":
+      try {
+        const locationData = request.body;
+        const location = new Location(locationData);
+        await location.save();
+        response.status(201).json({ status: "product created" });
+      } catch (e) {
+        console.error(e);
+        response.status(400).json({ error: e.message });
+      }
+      break;
+
     default:
       break;
   }
-
 }
